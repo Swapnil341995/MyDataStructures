2b624950ba4fba67d294d2fa7d3fed8aea66e670
swapnil341995 <sbharude.3@gmail.com>
2020-04-30 23:30:54 +0530

    Pattern Programming

---
+++ GitBST.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public :
+    int data;
+    node* left;
+    node* right;
+};
+
+int main()
+{
+    node n1,n2,n3,n4,n5,n6,n7;
+    n1.data = 10;
+    n1.left = NULL;
+    n1.right = NULL;
+
+    n2.data = 20;
+    n2.left = NULL;
+    n2.right = NULL;
+
+}
---
+++ GitQueue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#define MAX 5
+
+class Queue
+{
+    private :
+    
+};
+
+int main()
+{
+
+}
